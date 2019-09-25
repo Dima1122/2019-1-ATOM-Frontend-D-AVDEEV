2c5f0f52bd00db811fbb95cccd237e800c7a9d53
Dima1122 <dm-avdeev@yandex.ru>
2019-09-25 19:48:24 +0300

    Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Dmitry Avdeev123
+
---
+++ build/precache-manifest.b0a518f528a09de908a527f0bf51d221.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c899edaf50c739ae4fc6da3bbb5ea55",
+    "url": "/2019-2-ATOM-Frontend-D-AVDEEV/index.html"
+  },
+  {
+    "revision": "9f43c5e8ee8f9c0fb0e7",
+    "url": "/2019-2-ATOM-Frontend-D-AVDEEV/static/css/main.b4eae362.chunk.css"
+  },
+  {
+    "revision": "ae78a5a20ba1bedec88e",
+    "url": "/2019-2-ATOM-Frontend-D-AVDEEV/static/js/2.0b15456b.chunk.js"
+  },
+  {
+    "revision": "9f43c5e8ee8f9c0fb0e7",
+    "url": "/2019-2-ATOM-Frontend-D-AVDEEV/static/js/main.3ed57199.chunk.js"
+  },
+  {
+    "revision": "5d823bc9d5c673755277",
+    "url": "/2019-2-ATOM-Frontend-D-AVDEEV/static/js/runtime-main.e50131ca.js"
+  },
+  {
+    "revision": "a55f1c4358068374afaef45a15ab0eb8",
+    "url": "/2019-2-ATOM-Frontend-D-AVDEEV/static/media/logo.a55f1c43.svg"
+  }
+]);
